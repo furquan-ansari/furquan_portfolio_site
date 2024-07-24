966e42948bba5a14f5e87ab1d1ed5d470a4dea26
furquan-ansari <furquanansari274@gmail.com>
2024-07-24 13:53:40 +0530

    modified middleware

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Serve the static HTML file
-app.use(express.static("public"));
+app.use(express.static("FrontEnd"));
 
 // Route to handle form submission
 app.post("/send-email", (req, res) => {
