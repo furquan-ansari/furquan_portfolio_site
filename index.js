6b9d0a3e035c74ffe6112a347bcc4d91b136e127
furquan-ansari <furquanansari274@gmail.com>
2025-03-16 17:04:54 +0530

    assigned new port for deployed application

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 require('dotenv').config(); // To load environment variables from a .env file
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.DEPLOYED_PORT;
 
 // Middleware to parse form data
 app.use(bodyParser.urlencoded({ extended: false }));
